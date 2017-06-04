df8cd378dae382c6a6c6f3f9e9dbaa58782d65b5
sathiyaraj <sathiyaraj45@gmail.com>
2017-06-04 23:14:02 +0530

    web development

---
+++ js/jquery-func.js
@@ -0,0 +1,46 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+
+(12.941436, 80.209227)
+
+function initMap() {
+var uluru = {lat: 12.941436, lng: 80.209227};
+var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 18,
+  center: uluru
+});
+var marker = new google.maps.Marker({
+  position: uluru,
+  map: map,
+});
+}
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:false,
+    responsive:true,
+    items:1,
+    dots:false,
+    autoplay:true
+})
